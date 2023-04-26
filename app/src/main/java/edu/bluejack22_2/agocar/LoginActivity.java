69286e21d80359e1017d3d1606751a6f8080063f
anggaraswn <anggara11swn@gmail.com>
2023-04-26 21:08:40 +0700

    addIDInLoginRegister

--- app/src/main/java/edu/bluejack22_2/agocar/LoginActivity.java
+++ app/src/main/java/edu/bluejack22_2/agocar/LoginActivity.java
@@ -3,12 +3,25 @@ package edu.bluejack22_2.agocar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText emailField, passwordField;
+    Button loginBTN;
+    TextView forgotPassword, signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailField = findViewById(R.id.etEmail);
+        passwordField = findViewById(R.id.etPassword);
+        loginBTN = findViewById(R.id.btnLogin);
+        forgotPassword = findViewById(R.id.tvForgotPassword);
+        signUp = findViewById(R.id.tvSignUp);
     }
+
+
 }
