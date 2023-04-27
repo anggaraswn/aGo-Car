f1e8b8a32fed272d08dd1a99ee00d14a10328fc3
anggaraswn <anggara11swn@gmail.com>
2023-04-27 22:23:33 +0700

    referenceLayout

---
+++ app/src/main/java/edu/bluejack22_2/agocar/PreferencesActivity.java
@@ -0,0 +1,14 @@
+package edu.bluejack22_2.agocar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
+}
