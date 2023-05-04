d5eec8acad0d6e36b68f4e4bf781430773b1add1
anggaraswn <anggara11swn@gmail.com>
2023-05-04 16:48:19 +0700

    carsPageLayout

---
+++ app/src/main/java/edu/bluejack22_2/agocar/CarsActivity.java
@@ -0,0 +1,14 @@
+package edu.bluejack22_2.agocar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cars);
+    }
+}
